e5d90210b5f84dac595462f756c567bb9cb24d27
Kaz Matthews and Shruti Priya <kaz+shruti@>
2017-03-28 11:40:06 -0700

    add form for user to edit name and rating of first recipe.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,14 +7,28 @@ import { Component } from '@angular/core';
     <h1>Recipe Box</h1>
     <h3>A bunch of good recipes</h3>
     <ul>
-      <li *ngFor="let currentRecipe of recipes">{{currentRecipe.name}} - ingredients:
+      <li  [class] = "ratingColor(currentRecipe)" *ngFor="let currentRecipe of recipes">{{currentRecipe.name}} - ingredients:
         <ul>
           <li *ngFor="let currentIngredient of currentRecipe.ingredients">
           {{currentIngredient}}
           </li>
         </ul>
-       instructions: {{currentRecipe.instructions}}</li>
+       instructions: {{currentRecipe.instructions}}
+       <button (click)="editRecipe(currentRecipe)">Edit</button>
+      </li>
     </ul>
+    <div>
+      <h3>Edit Recipe</h3>
+       <label>Enter Recipe Name:</label>
+       <input [(ngModel)]="selectedRecipe.name">
+       <label>Enter Recipe Rating (1-5):</label>
+       <br>
+       <input type="radio" [(ngModel)]="selectedRecipe.rating" [value]="1">1<br>
+       <input type="radio" [(ngModel)]="selectedRecipe.rating" [value]="2">2<br>
+       <input type="radio" [(ngModel)]="selectedRecipe.rating" [value]="3">3<br>
+       <input type="radio" [(ngModel)]="selectedRecipe.rating" [value]="4">4<br>
+       <input type="radio" [(ngModel)]="selectedRecipe.rating" [value]="5">5<br>
+     </div>
   </div>
   `
 })
@@ -26,6 +40,23 @@ export class AppComponent {
     new Recipe('sweet potato burrito', ['salsa', 'sweet potato', 'cilantro', 'black beans', 'tortilla'], 'cook sweet potato and put everything inside of a tortilla', 5),
     new Recipe('grilled chicken', ['chicken', 'spices'], 'grill the chicken', 4)
   ];
+
+  selectedRecipe: Recipe = this.recipes[0];
+
+  editRecipe(clickedRecipe: Recipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  ratingColor(currentRecipe: Recipe){
+    if (currentRecipe.rating === 4){
+      return "bg-danger";
+    }
+    else {
+      return "bg-info";
+    }
+
+  }
+
 }
 
 export class Recipe {
